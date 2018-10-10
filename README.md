6edf3d36b90adfda1770eb49c4d08843b15620f8
wwang-nmdp <wwang@nmdp.org>
2018-10-10 10:21:01 -0500

    Update launcher with new options included. Debug slidingWindow chopping to filter variants-containing peptides only. Add references. Include more details of --help. compile executable ViTA-1.0.jar. Update readme.

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 Targeted genome editing has enormous potential for treating monogenic disorders, especially for those that affect the hematopoietic system. Reducing the immunogenicity of gene-editing components such as CRISPR-Cas9 is critical for ensuring safe treatment. Gene corrections may also result in novel epitopes of human leukocyte antigen (HLA). Here, we present an application, ViTA, that predicts immunogenicity of HLA-epitopes. 
 It supports common input formats, including single-variant chromosome coordinates or variant call format (VCF).
 ## Introduction
-CRISPR/Cas9 and other gene-editing technologies have exciting therapeutic potential. Clinical applications include curing monogenetic disorders (29930285) or humanizing animal organs for xenotransplantation (30233645). Considerable effort and progress has been made to reduce the immunogenicity of microbial-derived core editing components (ref) and delivery vectors (Chew, W. L. et al., 2018). However, relatively little attention is given to the possibility that somatic gene edits may lead to immunoreactive antigens introduced ex-post facto (Figure 1A). The effects could be counterproductive if immunocompetent patients reject the therapy or life threatening in cases where human leukocyte antigen (HLA)-bound peptides elicit an alloreactive-like response (ref). Here, we present a strategy paired with an efficient application, variant-to-antigen (ViTA), to simulate gene-edited peptide presentation. ViTA may aid strategies for the development of safe and effective gene therapies.
-## Algorithmic Flow Chart  
+CRISPR/Cas9 and other gene-editing technologies have exciting therapeutic potential. Clinical applications include curing monogenetic disorders or humanizing animal organs for xenotransplantation[1]. Considerable effort and progress has been made to reduce the immunogenicity of microbial-derived core editing components and delivery vectors[2]. However, relatively little attention is given to the possibility that somatic gene edits may lead to immunoreactive antigens introduced ex-post facto (Figure). The effects could be counterproductive if immunocompetent patients reject the therapy or life threatening in cases where human leukocyte antigen (HLA)-bound peptides elicit an alloreactive-like response[3]. Here, we present a strategy paired with an efficient application, variant-to-antigen (ViTA), to simulate gene-edited peptide presentation. ViTA may aid strategies for the development of safe and effective gene therapies.
+## Algorithmic Flow Chart
 
 ![AlgorithmicFlowChart](https://github.com/wwang-nmdp/ViTA/blob/master/doc/image/AlgorithmicFlowChart.png)
 
 ## Pre-installation
 
-### Install `SnpEff` from http://snpeff.sourceforge.net/index.html  SnpEff is a variant annotation and effect prediction tool. It annotates and predicts the effects of variants on genes (such as amino acid changes).
+### Install `SnpEff`[4] from http://snpeff.sourceforge.net/index.html  SnpEff is a variant annotation and effect prediction tool. It annotates and predicts the effects of variants on genes (such as amino acid changes).
 
-```unix 
+```unix
 wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
 unzip snpEff_latest_core.zip
 java -jar snpEff.jar
@@ -20,10 +20,10 @@ java -jar snpEff.jar
 
 #Usage: snpEff build [options] genome_version
 java -jar snpEff.jar build GRCh38.82
-### Install `netChop3.1` from Center For Biological Sequence Analysis, Technical University of Denmark.
-   The NetChop 3.1 may be downloaded only by special agreement.  For academic users there is a download site at:http://www.cbs.dtu.dk/cgi-bin/nph-sw_request?netchop. Other users are requested to contact   software@cbs.dtu.dk.   
+### Install `netChop3.1`[5] from Center For Biological Sequence Analysis, Technical University of Denmark.
+   The NetChop 3.1 may be downloaded only by special agreement.  For academic users there is a download site at:http://www.cbs.dtu.dk/cgi-bin/nph-sw_request?netchop. Other users are requested to contact   software@cbs.dtu.dk.
 
-```unix 
+```unix
 cat netchop-3.1.<unix>.tar.Z | uncompress | tar xvf -
 export NETCHOP=/full/path/to/netchop-3.1
 export TMPDIR=/full/path/to/netchop-3.1/tmp
@@ -33,13 +33,13 @@ In the 'netchop-3.1' directory test the software:
 bin/netchop test/test.fsa > test.out
   For research purpose, the net Chop3.1 is pre-built in MiHA identification pipeline. Make sure two environment variables, NETCHOPandTMDIR are set properly.Alsomakesurethat TMPDIR has the sticky bit set (the long listing should render 'drwxrwxrwt...'). If not, set it:
 
-```unix 
+```unix
 chmod 1777 $TMPDIR
 ```
-### Install `netMHCpan-3.0`
+### Install `netMHCpan-3.0`[6] from Center For Biological Sequence Analysis, Technical University of Denmark.
 Like netChop, the netMHCpan-3.0 may only be downloaded only by special agreement as well. In addition, it requests to download the data file (data.tar.gz) separately.
 
-```unix 
+```unix
 cat netMHCpan-3.0.<unix>.tar.gz | uncompress | tar xvf -
 
 cd ./netMHCpan-3.0
@@ -52,14 +52,14 @@ tar -xvf data.tar.gz
 
 ## Set environment variables
 
-```unix 
+```unix
 export TMPDIR=/Path/to/Tools/netMHCpan-3.0/tmp
 export NMHOME=/Path/to/Tools/netMHCpan-3.0/bin
 export NETMHCpan=/Path/to/Tools/netMHCpan-3.0
 ```
 
 
-## Build database of Transcriptome 
+## Build database of Transcriptome
 
 
 cds.db is pre-built hg38 transcriptome and minor allele frequence (version:snp147common) database, please download by using the provided link: https://sourceforge.net/projects/mihaip/files/cds.db.zip/download
@@ -75,4 +75,18 @@ Copy the database into your working directory.
 ```unix
 java -jar vita.jar -help
 #check options of the application and test with sample data
+
+
 ```
+# References:
+[1]   Meier, R.P., et al. Xenotransplantation: back to the future?. Transplant International, 2018.31(5): p.465-477.
+
+[2]   Chew WL., et al., Immunity to CRISPR Cas9 and Cas12a therapeutics. Wiley Interdisciplinary Reviews: Systems Biology and Medicine. 2018 Jan;10(1): p. 1408.
+
+[3]   Horowitz, M.M., et al., Graft-versus-leukemia reactions after bone marrow transplantation. Blood, 1990. 75(3): p. 555-62.
+
+[4]	Cingolani, P., et al., A program for annotating and predicting the effects of single nucleotide polymorphisms, SnpEff: SNPs in the genome of Drosophila melanogaster strain w1118; iso-2; iso-3. Fly (Austin), 2012. 6(2): p. 80-92.
+
+[5]	Kesmir, C., et al., Prediction of proteasome cleavage motifs by neural networks. Protein Eng, 2002. 15(4): p. 287-96.
+
+[6]	Nielsen, M. and M. Andreatta, NetMHCpan-3.0; improved prediction of binding to MHC class I molecules integrating information from multiple receptor and peptide length datasets. Genome Med, 2016. 8(1): p. 33.
--- src/main/java/com/nmdp/vita/Launcher.java
+++ src/main/java/com/nmdp/vita/Launcher.java
@@ -15,12 +15,60 @@ public class Launcher {
     private static final String OUTPUT= "-o";
     private static final String HELP = "-h";
     private static final String WINDOW = "-w";
-    private static final String CHROME = "-c";
-    private static final String LOCATION = "-l";
+    private static final String POS = "-pos";
     private static final String HLA_DIVIDER = ",";
-    
+    private static final String SHOW_VERSION = "-v";
+    private static final String VERSION = "1.0";
+
     private static boolean showHelp;
-    private static String helpInfo = "Note: TBD\n";
+    private static boolean showVersion;
+    private static String helpInfo = "Program: ViTA (variant to antigen)\n" +
+            "is a Java-based workflow for predicting the immunogenicity of HLA-epitopes\n" +
+            "by simulating gene-edited peptide(s) and their binding to patient HLA(s)\n" +
+            "Before running this program, make sure you have CDS.db downloaded on your working directory\n" +
+            "Usage: java -jar vita.jar <command> [option]\n" +
+            "A typical invovation would be:" +
+            "   java -jar vita.jar -i /path/to/myVariants.vcf -hla A02:01,B07:02,C04:02 -w -t /path/to/workingDirectory/Tools -o /path/to/output\n" +
+            "Commands:\n" +
+            "   -i,     Multiple or single variant input file\n" +
+            "           input file should be a tab-delimited format, regardless of the file extensions.\n\n" +
+            "   -pos,   Position of variants at chromosome coordinate\n" +
+            "           simply input the chromosome coordinate. e.g. -pos chr11:5227002\n" +
+            "           Be cautious, the position format have to be 1-based\n" +
+            "           Please follow the instruction of UCSC genome browser for details of coordinate system: \n" +
+            "               http://genome.ucsc.edu/blog/the-ucsc-genome-browser-coordinate-counting-systems/\n" +
+            "           It can only take either -i or -pos as input for each process\n\n" +
+            "   -hla,   List of HLA alleles you wish to access immunogenicities\n" +
+            "           HLA allele should be 4-digit resolution with one upper-case allele type followed by 4 numbers.\n" +
+            "               e.g. A01:01\n" +
+            "           Multiple allele input are allowed by comma-delimited format\n" +
+            "               e.g. A0101,A,2001,A0301,B0702\n" +
+            "           Depends on the number of CPUs and disk writing speed, multiple allele input could significantly reduce the predictions\n" +
+            "   -t,     Tell the program where are the dependency tools\n" +
+            "           Set the path to netChop and netMHCpan\n" +
+            "           You might also need to set the PATH for the dependencies:\n" +
+            "               export TMPDIR=/path/to/Tools/netMHCpan-3.0/tmp\n" +
+            "               export TMPDIR=/path/to/Tools/netchop-3.1/tmp\n" +
+            "               export NETCHOP=/path/to/Tools/netchop-3.1/\n" +
+            "               export NETMHCpan=/path/to/Tools/netMHCpan-3.0\n\n" +
+            "   -w,    \n" +
+            "           To activate the slidingWindow function for chopping out all possible variant-containing 8- to 11- mer\n" +
+            "           Otherwise the program goes netChop as default\n" +
+            "   -o,     Set a directory to store the output\n" +
+            "              e.g. /path/to/test/output\n" +
+            "           You don't have to make an exact directory for the program unless you need the output be somewhere specifically\n" +
+            "           Otherwise it will automatically generate one for you\n\n" +
+            "Miscellaneous:\n" +
+            "   -v,     Print current version information and exit\n" +
+            "   -h,     Print this help and exit\n\n" +
+            "Please check out latest update from our Github repo:\n" +
+            "https://github.com/wwang-nmdp/ViTA\n\n" +
+            "PROBLEMS:\n" +
+            "\n" +
+            "Leave the comments or report the issues via Github.\n" +
+            "   https://github.com/wwang-nmdp/ViTA/issues\n" +
+            "Contact wwang@nmdp.org in case of problems.\n" +
+            "\n";
     
    
     private static HashMap<String, String> paramMap = new HashMap<>();
@@ -30,7 +78,7 @@ public class Launcher {
         try{
             getParameters(args);
         }catch (IndexOutOfBoundsException e){
-            System.out.println("parameter is missing. program stopped");
+            System.out.println("Parameter is missing. program stopped");
             return;
         }
         //check if show help information or not
@@ -38,39 +86,62 @@ public class Launcher {
         	System.out.println(helpInfo);
         	return;
         }
+
+        if(showVersion){
+            System.out.println("The version is "+ VERSION);
+            return;
+        }
         
-        
-        if(paramMap.containsKey(INPUT)){
-            Configure.input = paramMap.get(INPUT);
-        }else {
-            Configure.chrome = paramMap.get(CHROME);
-            Configure.chrome = paramMap.get(LOCATION);
+        try{
+            if(paramMap.containsKey(INPUT)){
+                Configure.input = paramMap.get(INPUT);
+            }else if(paramMap.containsKey(POS)){
+                String pos = paramMap.get(POS);
+                String[] data = pos.split(":");
+                checkIllegalState(data.length != 2, "The format of position is not right");
+                checkIllegalState(data[0].startsWith("chr"), "The chrome should start with chr");
+                String chromeNumber = data[0].substring(3);
+                //Throw number format exception if it's not a number
+                Integer.parseInt(chromeNumber);
+                Configure.chrome = paramMap.get(chromeNumber);
+                Configure.location = paramMap.get(data[1]);
+            }
+
+
+            if(paramMap.containsKey(TOOL_FOLDER)){
+                Configure.tool = paramMap.get(TOOL_FOLDER);
+            }
+            checkIllegalState(paramMap.containsKey(OUTPUT), "The output folder is not set");
+            Configure.setOutputFolder(paramMap.get(OUTPUT));
+            checkIllegalState(paramMap.containsKey(HLA), "The HLA is not set");
+            Configure.setHLA(paramMap.get(HLA).split(HLA_DIVIDER));
+
+            FileHelp.makeFolders();
+
+            new SnpEff().run();
+            new Filter().run();
+            new TranslateToProtein().run();
+            if(Configure.windowSlide){
+                new WindowSlider().run();
+            }else{
+                new Cleavage().run();
+                new Chop().run();
+            }
+            new NetMHCpan().run();
+            new launchAff().run();
+        }catch (IllegalStateException exp){
+            System.out.println(exp.getMessage());
         }
 
-
-        if(paramMap.containsKey(TOOL_FOLDER)){
-        	 Configure.tool = paramMap.get(TOOL_FOLDER);
-        }
-        Configure.setOutputFolder(paramMap.get(OUTPUT));
-        Configure.setHLA(paramMap.get(HLA).split(HLA_DIVIDER));
-
-        FileHelp.makeFolders();
-
-        new SnpEff().run();
-        new Filter().run();
-        new TranslateToProtein().run();
-       if(Configure.windowSlide){
-            new WindowSlider().run();
-        }else{
-            new Cleavage().run();
-            new Chop().run();
-        }
-        new NetMHCpan().run();
-        new launchAff().run();
        
     }
 
 
+    private static void checkIllegalState(boolean goodState, String message){
+        if(!goodState){
+            throw new IllegalStateException(message);
+        }
+    }
 
     private static void getParameters(String[] args) {
     	//get window slide option
@@ -84,6 +155,14 @@ public class Launcher {
     	for (int i = 0; i < args.length; i++) {
             if (args[i].equals(HELP)) {
                 showHelp = true;
+                break;
+            }
+        }
+
+        //get version option
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(SHOW_VERSION)) {
+                showVersion = true;
                 break;
             }
         }
@@ -119,18 +198,10 @@ public class Launcher {
             }
         }
 
-        //get chrome
+        //get pos of chrome
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals(CHROME)) {
-                paramMap.put(CHROME, args[i+1]);
-                break;
-            }
-        }
-
-        //get location of chrome
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals(LOCATION)) {
-                paramMap.put(LOCATION, args[i+1]);
+            if (args[i].equals(POS)) {
+                paramMap.put(POS, args[i+1]);
                 break;
             }
         }
@@ -138,7 +209,7 @@ public class Launcher {
         //get hla strings
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals(HLA)) {
-                paramMap.put(LOCATION, args[i+1]);
+                paramMap.put(HLA, args[i+1]);
                 break;
             }
         }
